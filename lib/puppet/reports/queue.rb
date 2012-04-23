561fdb70ee3f6b74cbb837721c43121100fa3a1e
Hunter Haugen <h.haugen@gmail.com>
2012-04-23 16:03:05 -0700

    Report should use string keys not syms. Also targets are now hashes, not arrays

--- lib/puppet/reports/queue.rb
+++ lib/puppet/reports/queue.rb
@@ -10,8 +10,12 @@ Puppet::Reports.register_report(:queue) do
     config = YAML.load_file(configfile)
     # convert all the keys of each host entry to symbols
     begin
-      config[:hosts] = config[:hosts].map{|h|h.reduce({}){|h,(k,v)|h[k.to_sym]=v;h}}
-      send_msg(config[:hosts], config[:targets], report.to_json)
+      config["hosts"] = config["hosts"].map{|h|h.reduce({}){|h,(k,v)|h[k.to_sym]=v;h}}
+      config["targets"] = config["targets"].delete_if { |k,v|
+        v['type'] and v['type'] == 'file'
+      }.keys
+      p config
+      send_msg(config["hosts"], config["targets"], report.to_json)
     rescue => e
       p e
     end
